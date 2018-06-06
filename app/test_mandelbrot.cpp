2303e3d8fd8794a8c739057ec077dad2fa2fdeae
Tania Akter <tania9309@gmail.com>
2018-06-05 22:56:18 -0700

    temp commit to change implementation of is_member

--- app/test_mandelbrot.cpp
+++ app/test_mandelbrot.cpp
@@ -1,14 +1,13 @@
 //Author: Tania Akter, V00810640
-#include <iostream >
-#include " ra / mandelbrot . hpp "
+#include <iostream>
+#include "ra/mandelbrot.hpp"
 
 int main ()
 {
    // Force the image (in PNM format) to be computed at compile time.
-       constexpr auto s = ra :: fractal :: mandelbrot <256, 256>;
+       constexpr auto s = ra::fractal::mandelbrot <256, 256>;
   
        // Output the image (in PNM format).
-       std :: cout << s. begin () << ’\n ’;
-   }
-  
+       std :: cout << s. begin () << '\n';
+         
 }
--- include/ra/mandelbrot.hpp
+++ include/ra/mandelbrot.hpp
@@ -1,9 +1,76 @@
 //Author: Tania Akter, V00810640
-#ifndef
+#ifndef mandelbrot_hpp
 #define mandelbrot_hpp
+
+#include <utility>
+#include <ra/cexpr_basic_string.hpp>
 
 namespace ra{
 namespace fractal {
+
+    constexpr bool is_member(std::pair<double, double> complex)
+    {
+        std::pair<double, double> c = complex;
+        double before_re = complex.first();
+        double before_img = complex.second();
+        std::pair<double, double> before = complex;
+        for(int i = 1; i<16; i++)
+        {
+            
+            if( before.first * before.first + before.second * before.second > 4)
+            {
+                return false;
+            }
+            double re = ((before.first * before.first) - before.second*before.second) + c.first;
+            double img = 2*before.first*before.second + c.second;
+
+            before = std::make_pair(re, img);            
+        }
+        return true;
+    }
+
+
+    constexpr std::pair<double, double> lambda(std::size_t W , std::size_t H, double k, double l)
+    {
+        double a0 = -1.6;
+        double a1 = -1.1;
+        double b0 = 0.6;
+        double b1 = 1.1;
+
+        double real_n = b0 - a0;
+        double real_d = W-1;
+
+        double img_n = b1 - a1;
+        double img_d = H -1;
+
+        double real = a0 + k*(real_n/real_d);
+        double img = a1 + (H-1-l)*(img_n/img_d);
+        return std::make_pair(real,img);
+    }  
+    
+    template <std :: size_t W , std :: size_t H >    
+    constexpr ra::cexpr::cexpr_string<(W)*H+H+25> begin()
+    {
+        ra::cexpr::cexpr_string<(W)*H+H+25> str;
+        for(std::size_t i = 0; i< W; ++i)
+        {
+            for(std::size_t j = 0 ; j < H; ++j)
+            {
+                auto c = lambda(W,H,j,i);
+                if(is_member(c))
+                {
+                    str.push_back('1');
+                }
+                else
+                {
+                    str.push_back('0');
+                }
+            }
+            str.push_back('\n');
+        }
+
+        return str;
+    }
 
     // A variable template for a string that represents an image depicting
     // the Mandelbrot set. The image has width W and height H.
@@ -11,9 +78,7 @@ namespace fractal {
     // The string is a binary image encoded in the text-based bitmap PNM
     // format.
     template <std :: size_t W , std :: size_t H >
-    constexpr auto mandelbrot = implementation-defined;
-    
-
+    constexpr auto mandelbrot = begin<W,H>();
 }
 }
 
