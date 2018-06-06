9af482ff0c4df3bbd9ca320d6f75cbd3e7a05260
Tania Akter <tania9309@gmail.com>
2018-06-06 16:19:32 -0700

    implementation of filters
    Done implementation of biquad filter design function. More testing to be done in the next iteration

--- app/test_biquad_filter.cpp
+++ app/test_biquad_filter.cpp
@@ -1,1 +1,58 @@
 //Author: Tania Akter, V00810640
+
+#include "ra/biquad_filter.hpp"
+#include <iostream>
+
+int main() 
+{
+    using namespace ra::biquad;
+    using namespace std;
+
+    constexpr double f = 0.98;
+    constexpr double q = 0.7071;
+    constexpr auto filter = lowpass<double>(f,q);
+    cout << "Lowpass" << endl;
+    cout << "a0 " << filter.a0 << endl;
+    cout << "a1 " << filter.a1 << endl;
+    cout << "a2 " << filter.a2 << endl;
+    cout << "b0 " << filter.b0 << endl;
+    cout << "b1 " << filter.b1 << endl;
+    cout << "b2 " << filter.b2 << endl << endl;
+
+    constexpr auto h_filter = highpass<double>(f,q);
+    cout << "Highpass" << endl;
+    cout << "a0 " << h_filter.a0 << endl;
+    cout << "a1 " << h_filter.a1 << endl;
+    cout << "a2 " << h_filter.a2 << endl;
+    cout << "b0 " << h_filter.b0 << endl;
+    cout << "b1 " << h_filter.b1 << endl;
+    cout << "b2 " << h_filter.b2 << endl << endl;
+
+    constexpr auto bp_filter = bandpass<double>(f,q);
+    cout << "Bandpass" << endl;
+    cout << "a0 " << bp_filter.a0 << endl;
+    cout << "a1 " << bp_filter.a1 << endl;
+    cout << "a2 " << bp_filter.a2 << endl;
+    cout << "b0 " << bp_filter.b0 << endl;
+    cout << "b1 " << bp_filter.b1 << endl;
+    cout << "b2 " << bp_filter.b2 << endl << endl;
+
+    constexpr auto lsb_filter = lowshelf_boost<double>(f,double(1.5));
+    cout << "low-frequency shelving boost" << endl;
+    cout << "a0 " << lsb_filter.a0 << endl;
+    cout << "a1 " << lsb_filter.a1 << endl;
+    cout << "a2 " << lsb_filter.a2 << endl;
+    cout << "b0 " << lsb_filter.b0 << endl;
+    cout << "b1 " << lsb_filter.b1 << endl;
+    cout << "b2 " << lsb_filter.b2 << endl << endl;
+
+    constexpr auto lsc_filter = lowshelf_cut<double>(f,double(1.5));
+    cout << "low-frequency shelving cut" << endl;
+    cout << "a0 " << lsc_filter.a0 << endl;
+    cout << "a1 " << lsc_filter.a1 << endl;
+    cout << "a2 " << lsc_filter.a2 << endl;
+    cout << "b0 " << lsc_filter.b0 << endl;
+    cout << "b1 " << lsc_filter.b1 << endl;
+    cout << "b2 " << lsc_filter.b2 << endl << endl;
+    
+}
--- include/ra/biquad_filter.hpp
+++ include/ra/biquad_filter.hpp
@@ -1,8 +1,15 @@
-#Author: Tania Akter, V
+//Author: Tania Akter, V
 
-#ifndef BIQUAD_FILTER_H
-#define BIQUAD_FILTER_H
-namespace ra :: biquad {
+#ifndef biquad_filer_hpp
+#define biquad_filer_hpp
+
+#include "ra/cexpr_math.hpp"
+
+namespace ra{
+namespace biquad {
+
+    using namespace ra::cexpr_math;
+
     // Biquad filter coefficients class.
     template <class Real >
     struct biquad_filter_coefs
@@ -13,16 +20,26 @@ namespace ra :: biquad {
         // Creates a set of filter coefficients where the coefficients
         // a0, a1, a2, b0, b1, and b2 are initialized to a0_, a1_, a2_,
         // b0_, b1_, and b2_, respectively.
-        constexpr biquad_filter_coefs ( real a0_ , real a1_ , real a2_ , real b0_ ,
-                               real b1_ , real b2_ );
+        constexpr biquad_filter_coefs ( real a0_ , real a1_ , real a2_ , real b0_ , real b1_ , real b2_ ) :
+        a0(a0_), b0(b0_), a1(a1_), b1(b1_), a2(a2_), b2(b2_) {};
+                            
 
         // Creates a set of filter coefficients by copying from another set.
         // Since Real and OtherReal need not be the same, this constructor
         // can be used to convert between filter coefficients of different
         // types.
+        //TODO : check if it works as above
         template <class OtherReal >
         constexpr biquad_filter_coefs (
-                const biquad_filter_coefs < OtherReal >& coefs );
+                const biquad_filter_coefs < OtherReal >& coefs )
+                {
+                  a0 = static_cast<Real>(coefs.a0);
+                  a1 = static_cast<Real>(coefs.a1);
+                  a2 = static_cast<Real>(coefs.a2);
+                  b0 = static_cast<Real>(coefs.b0);
+                  b1 = static_cast<Real>(coefs.b1);
+                  b2 = static_cast<Real>(coefs.b2);
+                };
         // The filter coefficients a0, a1, a2, b0, b1, and b2.
         real a0 ;
         real a1 ;
@@ -33,13 +50,25 @@ namespace ra :: biquad {
     };
 
     // Returns the coefficients for a biquad lowpass filter with normalized
-        // cutoff frequency f and Q factor q, where f in [0,1] and q > 0.
+    // cutoff frequency f and Q factor q, where f in [0,1] and q > 0.
     // The filter coefficients are always normalized such that the
     // coefficient b0 is 1.
     // The type Real is a floating-point type.
     // All real arithmetic should be performed with the Real type.
     template <class Real >
-    constexpr biquad_filter_coefs < Real > lowpass ( Real f , Real q );
+    constexpr biquad_filter_coefs < Real > lowpass ( Real f , Real q )
+    {
+      Real omega = tan<Real>( (pi<Real>/(Real)2) * f);
+      
+      Real a0 = sqr<Real>(omega);
+      Real a1 = ((Real)2)*sqr<Real>(omega);
+      Real a2 = sqr<Real>(omega);
+      Real b0 = sqr<Real>(omega) + (omega / q )+ (Real)1;
+      Real b1 = ((Real)2)*(sqr<Real>(omega) - (Real)1);
+      Real b2 = sqr<Real>(omega) - (omega / q) +(Real)1;
+
+      return biquad_filter_coefs<Real>(a0/b0,a1/b0,a2/b0,b0/b0,b1/b0,b2/b0);
+    }
 
     // Returns the coefficients for a biquad highpass filter with
     // normalized cutoff frequency f and Q factor q, where f in [0,1]
@@ -49,7 +78,19 @@ namespace ra :: biquad {
     // The type Real is a floating-point type.
     // All real arithmetic should be performed with the Real type.
     template <class Real >
-    constexpr biquad_filter_coefs < Real > highpass ( Real f , Real q );
+    constexpr biquad_filter_coefs < Real > highpass ( Real f , Real q )
+    {
+      Real omega = tan<Real>( (pi<Real>/(Real)2) * f);
+
+      Real a0 = (Real)1;
+      Real a1 = (Real)-2;
+      Real a2 = (Real)1;
+      Real b0 = sqr<Real>(omega) + (omega / q )+ (Real)1;
+      Real b1 = ((Real)2)*(sqr<Real>(omega) - (Real)1);
+      Real b2 = sqr<Real>(omega) - (omega / q) +(Real)1;
+
+      return biquad_filter_coefs<Real>(a0/b0,a1/b0,a2/b0,b0/b0,b1/b0,b2/b0);
+    }
 
     // Returns the coefficients for a biquad bandpass filter with
     // normalized cutoff frequency f and Q factor q, where f in [0,1]
@@ -59,8 +100,20 @@ namespace ra :: biquad {
     // The type Real is a floating-point type.
     // All real arithmetic should be performed with the Real type.
     template <class Real >
-    constexpr biquad_filter_coefs < Real > bandpass ( Real f , Real q );
+    constexpr biquad_filter_coefs < Real > bandpass ( Real f , Real q )
+    {
+      Real omega = tan<Real>( (pi<Real>/(Real)2) * f);
+      
+      Real a0 = omega/q;
+      Real a1 = (Real)0;
+      Real a2 = -(omega/q);
+      Real b0 = sqr<Real>(omega) + (omega / q )+ (Real)1;
+      Real b1 = ((Real)2)*(sqr<Real>(omega) - (Real)1);
+      Real b2 = sqr<Real>(omega) - (omega / q) +(Real)1;
 
+      return biquad_filter_coefs<Real>(a0/b0,a1/b0,a2/b0,b0/b0,b1/b0,b2/b0);
+    }
+  
     // Returns the coefficients for a biquad low-frequency shelving
     // boost filter with normalized cutoff frequency f and gain-control
     // parameter a, where f in [0,1] and a >= 0.
@@ -70,8 +123,20 @@ namespace ra :: biquad {
     // The type Real is a floating-point type.
     // All real arithmetic should be performed with the Real type.
     template <class Real >
-    constexpr biquad_filter_coefs < Real > lowshelf_boost ( Real f , Real a );
+    constexpr biquad_filter_coefs < Real > lowshelf_boost ( Real f , Real a )
+    {
+      Real omega = tan<Real>( (pi<Real>/(Real)2) * f);
 
+      Real a0 = a*(sqr<Real>(omega)) + (sqrt<Real>((Real)2*a)*omega) + (Real)1;
+      Real a1 = (Real)2*(a*sqr<Real>(omega) - (Real)1);
+      Real a2 = a*(sqr<Real>(omega) ) - (sqrt<Real>((Real)2*a)*omega) + (Real)1;
+      Real b0 = sqr<Real>(omega) + ( sqrt<Real>((Real)2) * omega )+ (Real)1;
+      Real b1 = ((Real)2)*(sqr<Real>(omega) - (Real)1);
+      Real b2 = (sqr<Real>(omega) ) - (sqrt<Real>((Real)2)*omega) + (Real)1;;
+
+      return biquad_filter_coefs<Real>(a0/b0,a1/b0,a2/b0,b0/b0,b1/b0,b2/b0); 
+    }
+    //TODO: BOYANG
     // Returns the coefficients for a biquad low-frequency shelving
     // cut filter with normalized cutoff frequency f and gain-control
     // parameter a, where f in [0,1] and a >= 0.
@@ -81,6 +146,20 @@ namespace ra :: biquad {
     // The type Real is a floating-point type.
     // All real arithmetic should be performed with the Real type.
     template <class Real >
-    constexpr biquad_filter_coefs < Real > lowshelf_cut ( Real f , Real a );
+    constexpr biquad_filter_coefs < Real > lowshelf_cut ( Real f , Real a )
+    {
+      Real omega = tan<Real>( (pi<Real>/(Real)2) * f);
+
+      Real b0 = a*(sqr<Real>(omega)) + (sqrt<Real>((Real)2*a)*omega) + (Real)1;
+      Real b1 = (Real)2*(a*sqr<Real>(omega) - (Real)1);
+      Real b2 = a*(sqr<Real>(omega) ) - (sqrt<Real>((Real)2*a)*omega) + (Real)1;
+      Real a0 = sqr<Real>(omega) + ( sqrt<Real>((Real)2) * omega )+ (Real)1;
+      Real a1 = ((Real)2)*(sqr<Real>(omega) - (Real)1);
+      Real a2 = (sqr<Real>(omega) ) - (sqrt<Real>((Real)2)*omega) + (Real)1;;
+
+      return biquad_filter_coefs<Real>(a0/b0,a1/b0,a2/b0,b0/b0,b1/b0,b2/b0);
+    }
+    //TODO: ask Boyang
+}
 }
 #endif
